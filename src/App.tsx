1e6542300dc49b5fa3f1e54df5af6c1620a37877
vinaarthy <vinaandrianarisoa1@gmail.com>
2025-07-26 01:28:12 +0300

    chore: Initialisation du projet (Vite, Typescript, TailwindCSS, ShadCN UI, tailwindcss-animate, react-hook-form, zod, framer-motion, lucide-react) + configuration Tailwind, tsconfig paths, aliases ...

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="text-2xl font-bold text-blue-500">
+        Hello world !
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  	extend: {
+  		container: {
+  			center: true,
+  			padding: '1rem',
+  			screens: {
+  				'2xl': '1400px',
+  				xl: '1200px',
+  				lg: '1024px',
+  				md: '768px',
+  				sm: '640px',
+  				xs: '480px'
+  			}
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		}
+  	}
+  },
+  plugins: [
+	require("tailwindcss-animate")
+  ],
+}
+
