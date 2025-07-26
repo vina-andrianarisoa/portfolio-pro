487d68921e8e813147bb2204c93686d284c94268
vinaarthy <vinaandrianarisoa1@gmail.com>
2025-07-26 11:03:30 +0300

    chore: Initialisation du projet (Vite, Typescript, TailwindCSS, ShadCN UI, tailwindcss-animate, react-hook-form, zod, framer-motion, lucide-react) + configuration Tailwind, tsconfig paths, aliases ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
 
   return (
-    <>
-      <div className="text-2xl font-bold text-blue-500">
-        Hello world !
-      </div>
-    </>
+    <div className="flex flex-col min-h-screen justify-between">
+      <div>Header be mintsy ty</div>
+      <div className="flex-1">Main</div>
+      <div>Footer</div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,100 @@ export default {
   ],
   theme: {
   	extend: {
-		colors: {
-			border: 'var(--border)',
+		container: {
+			center: true,
+			padding: '1rem',
+			screens: {
+				'2xl': '1400px',
+				xl: '1200px',
+				lg: '1024px',
+				md: '768px',
+				sm: '640px',
+				xs: '480px'
+			}
 		},
-  		container: {
-  			center: true,
-  			padding: '1rem',
-  			screens: {
-  				'2xl': '1400px',
-  				xl: '1200px',
-  				lg: '1024px',
-  				md: '768px',
-  				sm: '640px',
-  				xs: '480px'
+		fontFamily: {
+			inter: [
+				'Inter',
+				'sans-serif'
+			],
+			roboto: [
+				'Roboto',
+				'sans-serif'
+			],
+			montserrat: [
+				'Montserrat',
+				'sans-serif'
+			],
+			sora: [
+				'Sora',
+				'sans-serif'
+			]
+		},
+		boxShadow: {
+			glow: '0 0 5px #a855f7, 0 0 10px #4338ca, 0px 0px 20px #a855f7, 0 0 25px #4338ca'
+		},
+		colors: {
+			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			dark: {
+  				DEFAULT: 'hsl(var(--dark))',
+  				foreground: 'hsl(var(--dark-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
-  		},
+		},
+		borderRadius: {
+			lg: 'var(--radius)',
+			md: 'calc(var(--radius) - 2px)',
+			sm: 'calc(var(--radius) - 4px)'
+		},
   		keyframes: {
   			'accordion-down': {
   				from: {
