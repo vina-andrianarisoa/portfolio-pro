4bb4b2e5f8e87ceb10404cb72cf2a4a345d5c57c
vinaarthy <vinaandrianarisoa1@gmail.com>
2025-08-02 21:28:59 +0300

    refactor: déplacement de index.css (global CSS Tailwind) dans le dossier styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '@/styles/index.css'
 import App from './App'
 import { ThemeProvider } from "@/components/ui/theme-provider"
 import '@fontsource/inter/400.css'
