ba8913c1af6da30f16a5ef35c747945fe2e9b13c
vinaarthy <vinaandrianarisoa1@gmail.com>
2025-08-02 12:07:48 +0300

    chore: Renommage du composant Navbar en Header et mise à jour des imports dans App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { Navbar } from '@/components/Navbar'
+import { Header } from '@/components/Header/Header'
 
 function App() {
 
   return (
     <div className="flex flex-col min-h-screen justify-between">
-      <header className="fixed top-0 left-0 w-full bg-transparent text-foreground shadow-md z-50">
-        <Navbar/>
-      </header>
-      
+      <Header/>
+    
       <main className="flex-1">
         <div>Main</div>
       </main>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,19 @@
-import { MenuDesktop } from '@/components/MenuDesktop'
-import { MenuMobile } from '@/components/MenuMobile'
+import { MenuDesktop } from '@/components/Header/MenuDesktop'
+import { MenuMobile } from '@/components/Header/MenuMobile'
 
-export const Navbar = () => {
+export const Header = () => {
 
     return (
-        <nav className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
-            <div className='text-xl font-bold'>VA</div>
-            
-            {/* Menu Desktop */}
-            <MenuDesktop/>
+        <header className="fixed top-0 left-0 w-full bg-transparent text-foreground shadow-md z-50">
+            <nav className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center">
+                <div className='text-xl font-bold'>VA</div>
+                
+                {/* Menu Desktop */}
+                <MenuDesktop/>
 
-            {/* Menu Mobile */}
-            <MenuMobile/>
-        </nav>
+                {/* Menu Mobile */}
+                <MenuMobile/>
+            </nav>
+        </header>
     )
 }
