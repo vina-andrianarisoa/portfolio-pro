5e04ae685bf833b6cd4ffe6220146debef41d74d
vinaarthy <vinaandrianarisoa1@gmail.com>
2025-08-19 12:57:52 +0300

    feat(Footer): Ajout de footerData.ts avec les liens sociaux et mail

---
+++ src/data/footerData.ts
@@ -0,0 +1,20 @@
+import { FooterLinks } from "@/types/footer"
+import { Github, Linkedin, Mail } from "lucide-react"
+
+export const footerLinks: FooterLinks[] = [
+    {
+        href: "https://github.com/vina-andrianarisoa",
+        label: "GitHub",
+        icon: Github,
+    },
+    {
+        href: "https://linkedin.com/in/vina-andrianarisoa",
+        label: "LinkedIn",
+        icon: Linkedin,
+    },
+    {
+        href: "mailto:vinaandrianarisoa1@gmail.com",
+        label: "Email",
+        icon: Mail,
+    },
+]
